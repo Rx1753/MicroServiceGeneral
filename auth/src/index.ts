b32b4e676f524ae4f78800d85a944d789d647012
Rx1753 <radixdt.1753@gmail.com>
2023-01-19 12:23:01 +0530

    first ts commit

---
+++ auth/src/index.ts
@@ -0,0 +1,24 @@
+import express from 'express';
+import 'express-async-errors';
+import bodyParser from 'body-parser';
+import cookieSession from 'cookie-session';
+
+const app = express();
+
+// The reason for this that traffic is being proxy to our app through ingress nginx
+app.set('trust proxy', true);
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    signed: false, // Disable encryption in cookie
+    secure: true, // use cookie only on https connection
+  })
+);
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log('listen at', port);
+});
+
